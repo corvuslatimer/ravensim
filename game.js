ad1f1bb7da292d84fc9c3afdf5262277a125f561
Corvus Latimer <corvuslatimer@gmail.com>
2026-03-06 08:59:33 +0000

    fix browser module resolution for three + GLTFLoader on static hosting

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js?module';
+import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js?module';
 
 const canvas = document.getElementById('game');
 const overlay = document.getElementById('overlay');
